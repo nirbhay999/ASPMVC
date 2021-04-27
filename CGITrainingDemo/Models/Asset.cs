20650dfa3a06ed8f73dea4cc6469bad4e8a7edad
nirbhay <nirbhay999@gmail.com>
2021-04-27 17:44:35 +0530

    Add validation

--- CGITrainingDemo/Models/Asset.cs
+++ CGITrainingDemo/Models/Asset.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,10 @@ namespace CGITrainingDemo.Models
     public class Asset
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Asset name is required")]
+        [MaxLength(30,ErrorMessage ="name cannot be more than 30 characters")]
         public string Name { get; set; }
+        [Required(ErrorMessage ="Asset description is required")]
         public string Description { get; set; }
     }
 }
