a3279dd2c0d3fc95e3293b19edbd14b88ae492c6
nirbhay <nirbhay999@gmail.com>
2021-04-29 16:50:37 +0530

    Custom tag helpers

---
+++ CGITrainingDemo/TagHelpers/SupportTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CGITrainingDemo.TagHelpers
+{
+    public class SupportTagHelper:TagHelper
+    {
+        public override void Process(TagHelperContext apple, TagHelperOutput mango)
+        {
+            mango.TagName = "div";
+            mango.Content.SetHtmlContent(@"<p>Contact Adress: 5th Avenue, Newyork, USA</p><p> Tel No: 9191919182882</p><p>Email:support @org.com</p>");
+        }
+    }
+}
